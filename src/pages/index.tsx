21aa07c602b80b000b0a8db85d07b5c93b2676d6
ruwaifatahir <66880814+ruwaifatahir@users.noreply.github.com>
2023-07-08 17:58:34 +0500

    add example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@
  * env convention
  * client and server side convention
  * role of .env.local, .env.development, .env.production
+ * .env.local.example, .env.production.example
  * how to add variables on vercel
  */
 function Home() {
@@ -19,4 +20,6 @@ function Home() {
 export default Home;
 
 const url = process.env.NEXT_PUBLIC_URL;
+const name = process.env.NEXT_PUBLIC_NAME;
 console.log("url:", url);
+console.log("name:", name);
