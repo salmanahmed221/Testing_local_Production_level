1defa65fcc42240ad6ea7ad92b7e1c8b5a5cf284
ruwaifatahir <66880814+ruwaifatahir@users.noreply.github.com>
2023-07-08 17:36:21 +0500

    add comments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
-"use client"
+"use client";
+
+/**
+ * env convention
+ * client and server side convention
+ * role of .env.local, .env.development, .env.production
+ * how to add variables on vercel
+ */
 function Home() {
-  return <div style={{ textAlign: "center", fontSize: "25px", backgroundColor: "red" }}>
-    Home Page
-  </div>;
+  return (
+    <div
+      style={{ textAlign: "center", fontSize: "25px", backgroundColor: "red" }}
+    >
+      Home Page
+    </div>
+  );
 }
 
 export default Home;
 
-const local = process.env.local_level;
-console.log(local);
+const url = process.env.NEXT_PUBLIC_URL;
+console.log("url:", url);
